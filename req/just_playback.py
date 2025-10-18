d1be578fe4b6fbc2ac31ef0a8d96199cdada7848
streitixy <danilomeiraivo@gmail.com>
2025-10-18 14:03:19 -0300

    teste

--- req/just_playback.py
+++
@@ -1,4 +0,0 @@
-from PyInstaller.utils.hooks import collect_all
-
-# coleta todos os arquivos, binários e imports do pacote just_playback
-datas, binaries, hiddenimports = collect_all('just_playback')
